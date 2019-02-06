661bb0507815b35f9118f1c0172985b4bd38c4b1
Irvin Velarde <irvinvelarde@gmail.com>
2019-02-06 15:07:51 -0500

    Añadiendo CSS

--- showmessagetxt/show.js
+++ showmessagetxt/show.js
@@ -1,4 +1,11 @@
 function ejucutarAJAX(){
+	/*
+		 0 => Peticion no ha sido inicialiazada
+		 1 => Peticion ha sido establecida
+		 2 => Peticion ha sido enviada
+		 3 => Peticion esta siendo procesada
+		 4 => Peticion ha sido finalizada
+	*/
 	var ajaxRequest = new XMLHttpRequest();
 	ajaxRequest.onreadystatechange = function(){
 		if (ajaxRequest.readyState == 4 && ajaxRequest.status == 200) {
